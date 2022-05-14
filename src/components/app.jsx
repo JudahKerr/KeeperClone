675c5df9ff373a4d6137f54089e6a8c43462a32c
Judah Kerr <mrjkerr87@gmail.com>
2022-05-14 12:44:59 -0700

    Complete Keeper Note functionallity

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [noteArray, setNoteArray] = useState([]);
+
+  function addNote(note) {
+    setNoteArray((prevNote) => {
+      return [...prevNote, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNoteArray((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {noteArray.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            delete={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
